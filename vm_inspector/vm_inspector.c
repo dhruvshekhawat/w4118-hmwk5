906b07aca8e226168f6de36e424e078ad5725503
Andrei Papancea <alp2200@columbia.edu>
2014-11-15 18:26:56 -0500

    cleanup and error checking of initial vm_inspector.c

--- vm_inspector/vm_inspector.c
+++ vm_inspector/vm_inspector.c
@@ -1,49 +1,58 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
-static int isNumeric (const char * s)
+static int is_numeric(const char *s)
 {
+	char *p;
+
 	if (s == NULL || *s == '\0' || isspace(*s))
 		return 0;
-	char * p;
-	strtod (s, &p);
+	strtod(s, &p);
+
 	return *p == '\0';
+}
+
+static int is_verbose(char *arg)
+{
+	return strlen(arg) == 2 && strncmp(arg, "-v", 2) == 0;
 }
 
 int main(int argc, char **argv)
 {
-	bool verbose = false;
 	int pid = -1;
-	char *verbose ="-v";
-	if (argc > 2) {
+	int vflag = 0;
+
+	if (argc != 2 && argc != 3) {
 		printf("Usage:%s [-v] [pid]\n", argv[0]);
 		return 1;
 	}
-	if (argc  == 1) {
-		if (strcmp(argv[1],verbose) == 0) {
-			verbose = true;
-		} else if (isNumeric(argv[1])) {
+
+	if (argc == 2) {
+		if (is_numeric(argv[1])) {
 			pid = atoi(argv[1]);
 		} else {
 			 printf("Usage:%s [-v] [pid]\n", argv[0]);
 			 return 1;
 		}
-	} else if (argc == 2) {
-		if (strcmp(argv[1],verbose) == 0 &&
-		    isNumeric(argv[2])) {
-			verbose = true;
-			pid = atoi(argv[2]);
-		} else if (strcmp(argv[2],verbose) == 0 &&
-			   isNumeric(argv[2])) {
-			verbose = true;
-			 pid = atoi(argv[2]);
-		} else
-			printf("Usage:%s [-v] pid\n", argv[0]);
+	} else if (argc == 3) {
+		if (is_numeric(argv[1]) && is_verbose(argv[2])) {
+			vflag = 1;
+			pid = atoi(argv[1]);
+		} else if (is_verbose(argv[1]) && is_numeric(argv[2])) {
+			vflag = 1;
+			pid = atoi(argv[1]);
+		} else {
+			printf("usage: %s [-v] [pid]\n", argv[0]);
+			return -1;
+		}
+
+		printf("verbose\n");
 	}
-	
+
 	printf("shit\n");
 	return 0;
 }
