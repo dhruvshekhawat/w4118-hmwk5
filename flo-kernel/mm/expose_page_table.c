340ce1626641450cf3d9b74af3cc3be621ec0c31
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2014-11-15 18:15:19 -0500

    Adding template for our callback

--- flo-kernel/mm/expose_page_table.c
+++ flo-kernel/mm/expose_page_table.c
@@ -5,11 +5,26 @@
  * COMS W4118 implementation of syscall for page table
  * exposure in user space for the current process.
  */
-
 #include <asm-generic/errno-base.h>
 #include <linux/syscalls.h>
 #include <asm/uaccess.h>
 #include <asm/current.h>
+
+
+/* Helper to...
+ *
+ * @pmd:  
+ * @addr: starting address
+ * @end:  ending address
+ * @walk: set of callbacks to invoke for each level of the tree
+ *
+ */
+static int remap_pte(pmd_t *pmd, unsigned long addr,
+		     unsigned long end, struct mm_walk *walk)
+{
+
+}
+
 
 /* Map a target process's page table into address space of the current process.
  *
@@ -24,30 +39,29 @@
  * @fake_pgd: base address of the fake pgd table
  * @addr: base address in the user space that the page tables should map to
  */
-SYSCALL_DEFINE3(expose_page_table,
-				pid_t, pid,
-				unsigned long, fake_pgd,
-				unsigned long, addr)
+SYSCALL_DEFINE3(expose_page_table, pid_t, pid, unsigned long, fake_pgd,
+		unsigned long, addr)
 {
-
 	int ret_code = 0;
 	struct task_struct *tsk;
 	struct mm_struct *tsk_mm;
 	struct vm_area_struct *tsk_vma;
-	struct mm_walk page_walk = {};
+
+	struct mm_walk walk = {
+		.mm = tsk->mm,
+		.pde_entry = remap_pte,
+		.private = vma,
+	};
 
 	if (addr & ~PAGE_MASK || fake_pgd & ~PAGE_MASK)
 		return -EINVAL;
 
-	if (pid == -1)
-		tsk = find_task_by_vpid(current->pid);
-	else
-		tsk = find_task_by_vpid(pid);
+	tsk = pid == -1 ? current : find_task_by_vpid(pid)
 	if (tsk == NULL)
 		return -EINVAL;
 
 	down_write(tsk_mm->mmap_sem);
-	tsk_mm = tsk->>mm;
+	tsk_mm = tsk->mm;
 	vma = find_vma(tsk_mm,addr);
 	if (vma == NULL) {
 		ret_code = -EFAULT;
@@ -55,10 +69,8 @@ SYSCALL_DEFINE3(expose_page_table,
 	}
 
 	vma->vm_flags |= VM_SPECIAL;
-	page_walk.mm = mm;
-	page_walk.private = vma;
-
-	ret_code = walk_page_range(0, , &page_walk);
+	
+	ret_code = walk_page_range(0, , &remap_pte);
 
 	return ret_code;
 
