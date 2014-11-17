7416f370bcc7a6a5b7e301ca9d4bdbf23cd210d7
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2014-11-17 13:01:11 -0500

    Move expose_page_table.h to includes

---
+++ flo-kernel/include/linux/expose_page_table.h
@@ -0,0 +1,169 @@
+/*
+ * kernel/expose_page_table.c
+ * Copyright (C) 2014 V. Atlidakis, G. Koloventzos, A. Papancea
+ *
+ * COMS W4118 implementation of syscall for page table
+ * exposure in user space for the current process.
+ */
+
+/*
+ * The structural overview of fake pgd and remapped pte is the following
+ * in which fake pdg indexes remmaped pte.
+ *
+ *   fake pdg          remapped pte
+ * |     0     |----> +------------+ Remapped Address of Page Table 0  
+ * +-----------+      |            | 
+ * |     1     |---   |            |
+ * +-----------+  |   +------------+ 
+ * |     2     |  |   |     '      |
+ * +-----------+  |   |     '      |
+ * |     3     |  |   |     '      |
+ * +-----------+  |   |     '      |
+ * |     4     |  --> +------------+ Remapped Address of Page Table  1
+ * +-----------+      |     '      |
+ * |           |      |     '      |
+ *       '                  '
+ *       '                  '
+ *
+ * The size of the mmape'ed space must be sufficient to hold  4096
+ * remapped pte ( 2048 * 4096 = 8 * 1024 * 1024 = 8 MB)
+ *
+ *
+ *
+ */
+#define PAGE_SIZE				(4 * 1024)
+#define EXPOSED_TABLE_SIZE			(8 * 1024 * 1024)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
